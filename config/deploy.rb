b6b3ef72f87aafa2068b3d3fb35811732fe4e59a
Waseem Ahmad <talk.to.waseem@gmail.com>
2018-01-05 02:20:17 +0530

    Add .env to linked files for shopify api credentials.

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ set :keep_assets, 5
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", "log/nginx.access.log", "log/nginx.error.log"
+append :linked_files, "config/database.yml", "config/secrets.yml", "log/nginx.access.log", "log/nginx.error.log", ".env"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "vendor/bundle", "tmp/sockets", "public/system"
